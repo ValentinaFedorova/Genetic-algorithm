97efe2d6b9b21915875014b29582c0894bddb4e8
ValentinaFedorova <57736319+ValentinaFedorova@users.noreply.github.com>
2020-03-30 18:54:40 +0800

    Update gen_alg.py

--- gen_alg.py
+++ gen_alg.py
@@ -1,13 +1,6 @@
 import math
 import random
 
-class Chromosome:
-        def __init__(self, x1, x2, g):
-            self.x1 = x1
-            self.x2 = x2
-            self.g = g
-        def __repr__(self):
-            return repr((self.x1, self.x2, self.g))
         
 def myFunc(e):
   return e['g']
@@ -76,97 +69,5 @@ result2 = gen_algorithm(-2,2,-2,2,goldsteinprice_func)
 print("Точка минимума для функции Гольдман-Прайса: [",result2[0]," , ",result2[1],"]")
 print("Значение в точке минимума: ", result2[2])
 
-# print(mccormick_function(-0.54719,-1.54719))
 
-# low_limit_x1 = -1.5
-# high_limit_x1 = 4
-# low_limit_x2 = -3
-# high_limit_x2 = 4
-
-# chromosomes = []
-# for i in range(0,4):
-#     x1=random.uniform(low_limit_x1,high_limit_x1)
-#     x2 = random.uniform(low_limit_x2,high_limit_x2)
-#     g = mccormick_function(x1,x2)
-#     chromosomes.append({'x1':x1,'x2':x2,'g':g})
-# chromosomes.sort(key=myFunc)
-
-
-
-# cnt = 0
-# minimum = chromosomes[0]['g']
-# while cnt<1000000:
-    
-
-#     next_generation = []
-
-#     for i in range(1,len(chromosomes)-1):
-#         next_generation.append({'x1':chromosomes[0]['x1'],'x2':chromosomes[i]['x2']})
-#         next_generation.append({'x1':chromosomes[i]['x1'],'x2':chromosomes[0]['x2']})
-
-#     next_generation[3]['x1'] = random.uniform(low_limit_x1,high_limit_x1)
-#     next_generation[3]['x2'] = random.uniform(low_limit_x2,high_limit_x2)
-#     for i in range(0,len(next_generation)):
-#         next_generation[i]['g'] = mccormick_function(next_generation[i]['x1'],next_generation[i]['x2'])
-
-#     chromosomes = next_generation
-
-#     chromosomes.sort(key=myFunc)
-     
-
-    
-#     current_min = chromosomes[0]['g']
-#     if minimum > current_min:
-#         minimum = current_min
-
-#     cnt = cnt + 1 
-
-# print("Точка минимума для функции МакКормика: [",chromosomes[0]['x1']," , ",chromosomes[0]['x2'],"]")
-# print("Значение в точке минимума: ", minimum)
-
-# low_limit_x1 = -2
-# high_limit_x1 = 2
-# low_limit_x2 = -2
-# high_limit_x2 = 2
-
-# chromosomes = []
-# for i in range(0,4):
-#     x1=random.uniform(low_limit_x1,high_limit_x1)
-#     x2 = random.uniform(low_limit_x2,high_limit_x2)
-#     g = goldsteinprice_func(x1,x2)
-#     chromosomes.append({'x1':x1,'x2':x2,'g':g})
-# chromosomes.sort(key=myFunc)
-
-
-
-# cnt = 0
-# minimum = chromosomes[0]['g']
-# while cnt<1000000:
-    
-
-#     next_generation = []
-
-#     for i in range(1,len(chromosomes)-1):
-#         next_generation.append({'x1':chromosomes[0]['x1'],'x2':chromosomes[i]['x2']})
-#         next_generation.append({'x1':chromosomes[i]['x1'],'x2':chromosomes[0]['x2']})
-
-#     next_generation[3]['x1'] = random.uniform(low_limit_x1,high_limit_x1)
-#     next_generation[3]['x2'] = random.uniform(low_limit_x2,high_limit_x2)
-#     for i in range(0,len(next_generation)):
-#         next_generation[i]['g'] = goldsteinprice_func(next_generation[i]['x1'],next_generation[i]['x2'])
-
-#     chromosomes = next_generation
-
-#     chromosomes.sort(key=myFunc)
-     
-
-    
-#     current_min = chromosomes[0]['g']
-#     if minimum > current_min:
-#         minimum = current_min
-
-#     cnt = cnt + 1 
-
-# print("Точка минимума для функции Гольдман-Прайса: [",chromosomes[0]['x1']," , ",chromosomes[0]['x2'],"]")
-# print("Значение в точке минимума: ", minimum)
 
